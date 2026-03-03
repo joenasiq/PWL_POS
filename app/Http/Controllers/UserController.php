5df9109edd1c1c68de95e3f7f3ac2bed8febb031
arjuna <arjunash2006@gmail.com>
2026-03-03 17:29:53 +0700

    Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,16 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
-        return view('user', ['data' => $user]);
+        $data = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2,
+            ],
+        );
+        $data->save();
+        
+        return view('user', ['data' => $data]);
     }
 }
